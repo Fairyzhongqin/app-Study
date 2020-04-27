aef7516d7f8876b72b7f23a89836aa93d04a6edc
fairychen <5147454+fairychen@user.noreply.gitee.com>
2020-04-27 22:44:23 +0800

    厨师文件

---
+++ pages/contact/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view>这是联系我们页面</view>
+		<view>
+			<text selectable>这是text组件</text>
+		</view>
+		<view class="one" hover-class="hover-one">有Hover样式</view>
+	</view>
+	
+</template>
+
+<script>
+</script>
+
+<style>
+	.one {
+		width: 100px;
+		height: 100px;
+		border:1px solid red;
+	}
+	.hover-one {
+		background-color: red;
+	}
+</style>
